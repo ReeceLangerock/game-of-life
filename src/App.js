efdaac2baf65a25e828e8551dccd8753dc15ca0a
ReeceLangerock <ReeceLangerock@gmail.com>
2017-09-10 19:58:21 -0500

    force 100vh, prevent mousescroll, add div to force board to middle

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import styled from "styled-components";
-import Board from './components/Board'
+import Board from "./components/Board";
+import Footer from './components/Footer'
 
 import "./App.css";
 
@@ -11,21 +12,31 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   max-width: 100vw;
+  max-height: 100vh;
   overflow: hidden;
   position: relative;
   background-color: #f7f7f7;
 `;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // page is at max 100vh but mouse scroll was still causing react to rerender components
+    // causing the game of life simulation to lag, so stop that from registering
+    window.onwheel = function() {
+      return false;
+    };
+  }
+
   render() {
     return (
       <Provider store={store}>
         <Container className="App">
-     
-         <Board/>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          {/* Extra div to force board to middle of page */}
+          <div/>
+          <Board />
+          <Footer/>
         </Container>
       </Provider>
     );
