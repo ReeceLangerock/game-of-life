ad312f900eb4292905a0e8bcc87a07d97a982f22
ReeceLangerock <ReeceLangerock@gmail.com>
2017-09-10 20:01:24 -0500

    update cell style, allow all to update when resizing

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,23 +1,20 @@
 import React from "react";
-import styled, { injectGlobal } from "styled-components";
+import styled from "styled-components";
 
 const StyledCell = styled.div`
-  width: ${props => 500 / props.size - 1}px;
-  height: ${props => 500 / props.size - 1}px;
-
-  // width: 6px;
-  // height: 6px;
-  border-top: 1px solid;
-  border-left: 1px solid;
+  width: ${props => 600 / props.size - 1}px;
+  height: ${props => 600 / props.size - 1}px;
+  border-top: 1px solid #697273;
+  border-left: 1px solid #697273;
   border-collapse: collapse;
-  background: ${props => (props.status ? "green" : "white")};
+  background: ${props => (props.status ? "#80CF7C" : "white")};
 `;
 
 export class Cell extends React.Component {
-
+  //prevent cell from re-rendering if alive state is unchanged
   shouldComponentUpdate(nextProps) {
-    return nextProps.status !== this.props.status
-}
+    return nextProps.size === this.props.size ? nextProps.status !== this.props.status : true;
+  }
   render() {
     return <StyledCell size={this.props.size} status={this.props.status} onClick={this.props.onClick} />;
   }
